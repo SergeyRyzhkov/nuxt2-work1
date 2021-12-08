864140b85ae12f7c9beec0d310e5f6c2bd5ec370
SergeyRyzhkov <SergeyRyzhkov76@gmail.com>
2021-12-08 13:53:18 +0300

    fix

--- src/modules/Root/pages/MainPage.vue
+++ src/modules/Root/pages/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="page-wrapper">
     <section class="bg-nude container-fluid">
-      <div class="container bg-nude"><img src="/images/_tmp/1.jpg" loading="lazy" /></div>
+      <div class="container bg-nude"><img src="/images/_tmp/1.jpg" width="1350" height="831" /></div>
     </section>
   </main>
 </template>
