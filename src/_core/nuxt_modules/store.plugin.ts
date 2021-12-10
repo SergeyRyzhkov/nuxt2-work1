27e931d56494095dfac1218c583a1613a214e973
SergeyRyzhkov <SergeyRyzhkov76@gmail.com>
2021-12-10 04:57:42 +0300

    rrr

--- src/_core/nuxt_modules/store.plugin.ts
+++ src/_core/nuxt_modules/store.plugin.ts
@@ -1,29 +1,12 @@
 const modulesImport = JSON.parse(`<%= JSON.stringify(options.modulesImport) %>`);
 
 export default async ({ store }) => {
-  // const modulesPr = getModulesPromises();
-  // const modules = await Promise.all(modulesPr);
-
-  const modules = await getModulesPromises();
-
-  modules.forEach((iter: any) => {
-    if (!!iter && iter.name) {
-      store.registerModule(iter.name, iter);
-    }
-  });
-};
-
-const getModulesPromises = async () => {
-  const modules = [];
   if (!!modulesImport) {
     for (const iter of modulesImport) {
       const part = iter.split("modules")[1];
-      // @ts-ignore
-      modules.push(await lazyLoad(import(`../src/modules${part}`)));
+      store.registerModule(iter.name, await lazyLoad(import(`../src/modules${part}`)));
     }
   }
-
-  return modules;
 };
 
 export const lazyLoad = (promise) => {
--- src/plugins/app-init.ts
+++ src/plugins/app-init.ts
@@ -28,7 +28,7 @@ export default async (ctx: Context, inject: Inject) => {
   // @ts-ignore
   ctx.store.registerModule("test", await lazyLoad(import("@/modules/Auth/store/Test.ts")));
   // @ts-ignore
-  ctx.store.registerModule("AppStore", await lazyLoad(import("@/modules/Root/store/AppStore.ts")));
+  // ctx.store.registerModule("AppStore", await lazyLoad(import("@/modules/Root/store/AppStore.ts")));
 
   inject("serviceLocator", ServiceLocator.instance);
 };
