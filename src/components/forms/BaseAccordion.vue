5f213a92efaff8ed078c6084dcc127635106449a
SergeyRyzhkov <SergeyRyzhkov76@gmail.com>
2021-12-28 22:42:02 +0300

    add: product accordions

---
+++ src/components/forms/BaseAccordion.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="base-accordion">
+    <div class="flex font-semibold text-18 justify-between items-center cursor-pointer" @click="toogle()">
+      <slot name="header"></slot>
+      <span v-show="!isContentActive" class="text-28 font-normal">+</span>
+      <span v-show="isContentActive" class="text-28 font-normal">-</span>
+    </div>
+    <div class="base-accordion__content" :class="[isContentActive ? 'active' : '']">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class BaseAccordion extends Vue {
+  isContentActive = false;
+
+  toogle() {
+    this.isContentActive = !this.isContentActive;
+  }
+}
+</script>
+
+<style lang="scss">
+.base-accordion {
+  border-bottom: 1px solid #c9c9c9;
+  padding: 18px 0px 18px 0px;
+  > span {
+    transition: 0.2s;
+    line-height: 0px;
+    outline: none;
+  }
+  &__content {
+    transition: 0.2s;
+    max-height: 0px;
+    overflow-y: hidden;
+    margin-top: 0px;
+    &.active {
+      max-height: 300px;
+      margin-top: 16px;
+    }
+  }
+}
+</style>
--- src/modules/Training/components/ProgrammDay.vue
+++ src/modules/Training/components/ProgrammDay.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="programm-day">
-    <div class="flex font-semibold text-18 justify-between items-center cursor-pointer" @click="toogle()">
+  <BaseAccordion>
+    <template #header>
       <span>{{ dayNumber + " день. " + day.name }}</span>
-      <span v-show="!isContentActive" class="text-28 font-normal">+</span>
-      <span v-show="isContentActive" class="text-28 font-normal">-</span>
-    </div>
-    <div class="programm-day__content" :class="[isContentActive ? 'active' : '']" v-html="day.content"></div>
-  </div>
+    </template>
+    <template #content><div class="text-14" v-html="day.content"></div></template>
+  </BaseAccordion>
 </template>
 
 <script lang="ts">
@@ -19,34 +17,5 @@ export default class ProgrammDay extends Vue {
 
   @Prop({ default: 1 })
   dayNumber;
-
-  isContentActive = false;
-
-  toogle() {
-    this.isContentActive = !this.isContentActive;
-  }
 }
 </script>
-
-<style lang="scss">
-.programm-day {
-  border-bottom: 1px solid #c9c9c9;
-  padding: 18px 0px 18px 0px;
-  > span {
-    transition: 0.2s;
-    line-height: 0px;
-    outline: none;
-  }
-  &__content {
-    transition: 0.2s;
-    max-height: 0px;
-    overflow-y: hidden;
-    font-size: 14px;
-    margin-top: 0px;
-    &.active {
-      max-height: 300px;
-      margin-top: 16px;
-    }
-  }
-}
-</style>
