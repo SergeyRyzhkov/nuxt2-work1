461639a7e18d2742cfc7bb2588ea14ed546b0365
SergeyRyzhkov <SergeyRyzhkov76@gmail.com>
2021-12-28 09:53:45 +0300

    add: reset password

---
+++ src/modules/Auth/pages/ResetPassword.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="h-100"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "nuxt-property-decorator";
+import { EmptyService } from "@/_core/service/EmptyService";
+
+@Component
+export default class ResetPassword extends Vue {
+  @Prop()
+  code;
+
+  async mounted() {
+    try {
+      await this.$serviceLocator.getService(EmptyService).getAnyOrFail(`users/password/recover/${this.code}`);
+    } catch (err) {
+      console.log(err);
+    }
+
+    this.$router.push({ name: "personal" });
+  }
+}
+</script>
+
+<style lang="scss"></style>
