3681628528282d70df84a2772013125b9382e765
Kirill Ovodov <ovodovkirill1999@gmail.com>
2022-01-12 17:48:25 +0300

    fix cookies

--- src/components/CookieComponent.vue
+++ src/components/CookieComponent.vue
@@ -7,11 +7,15 @@
         </picture>
       </figure>
       <div class="ml-14 flex flex-col sm:flex-row sm:items-center">
-        <div class="whitespace-nowrap flex-col sm:flex-row flex text-16 tracking-wide">Мы используем Cookies. <a href="javascript:void(0)" class="link-hover ml-4"> Читать далее</a></div>
+        <div class="whitespace-nowrap flex-col sm:flex-row flex text-16 tracking-wide">Мы используем Cookies. <a
+          href="javascript:void(0)" class="link-hover ml-4"> Читать далее</a></div>
         <BaseButton class="sm:ml-20 mt-16 sm:mt-0 cookie-btn" @click="setCookie">я согласен</BaseButton>
-        <svg @click="visible = false" class="sm:ml-18 close-icon" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect width="15.1591" height="1.51591" rx="0.757954" transform="matrix(0.701006 0.713155 -0.701006 0.713155 1.0625 0.108887)" fill="white"/>
-          <rect width="15.1591" height="1.51591" rx="0.757954" transform="matrix(0.701006 -0.713155 0.701006 0.713155 0.310547 10.8108)" fill="white"/>
+        <svg @click="visible = false" class="sm:ml-18 close-icon" width="12" height="12" viewBox="0 0 12 12" fill="none"
+             xmlns="http://www.w3.org/2000/svg">
+          <rect width="15.1591" height="1.51591" rx="0.757954"
+                transform="matrix(0.701006 0.713155 -0.701006 0.713155 1.0625 0.108887)" fill="white"/>
+          <rect width="15.1591" height="1.51591" rx="0.757954"
+                transform="matrix(0.701006 -0.713155 0.701006 0.713155 0.310547 10.8108)" fill="white"/>
         </svg>
       </div>
     </div>
@@ -22,31 +26,33 @@
 import {Component, Vue} from "nuxt-property-decorator";
 
 @Component
-export default  class CookieComponent extends Vue{
+export default class CookieComponent extends Vue {
   visible: boolean = false
 
-  get isVisible(){
-    // const isAccepted = this.$cookies.get('useCookies')
-    // return this.visible && !isAccepted
-    return false;
+  get isVisible() {
+    //@ts-ignore
+    const isAccepted = this.$cookies.get('useCookies')
+    return this.visible && !isAccepted
   }
 
   fetch() {
-    // const isAccepted = this.$cookies.get('useCookies')
-    // if (!isAccepted){
-    //   this.visible = true
-    // }
+    //@ts-ignore
+    const isAccepted = this.$cookies.get('useCookies')
+    if (!isAccepted) {
+      this.visible = true
+    }
   }
 
-  setCookie(){
-      // this.$cookies.set('useCookies', true)
+  setCookie() {
+    //@ts-ignore
+    this.$cookies.set('useCookies', true)
     this.visible = false
   }
 }
 </script>
 
 <style lang="scss">
-.cookie-component{
+.cookie-component {
   z-index: 10000;
   position: fixed;
   width: 100%;
@@ -60,22 +66,25 @@ export default  class CookieComponent extends Vue{
     max-width: 600px;
     bottom: 32px;
   }
-  .cookie-image{
+
+  .cookie-image {
     width: 40px;
     height: 40px;
   }
-  .close-icon{
-      width: 12px;
+
+  .close-icon {
+    width: 12px;
     height: 12px;
     right: 24px;
     top: 24px;
     position: absolute;
     @include mobile {
-        position: static;
+      position: static;
     }
     cursor: pointer;
   }
-  .cookie-btn{
+
+  .cookie-btn {
     background: #ffffff;
     color: black;
     padding: 10px !important;
